0dcc62e3fda9db56317dc72e4ab3038ca8161781
Sam De Francesco <sam@defrances.co>
2014-01-15 08:53:04 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Khatm::Application.config.secret_key_base = 'd8016b122e4318ced415dc67cea8b71cb45a861b2045f34e23c6ede8ee901e27ea8e30eac6ddf5fa21ce066186ca45990193f06ce38eb16f08bf8717fc6755f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Khatm::Application.config.session_store :cookie_store, key: '_khatm_session'
